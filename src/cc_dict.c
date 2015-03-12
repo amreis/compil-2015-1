d5048c0b3f01c1906b8f432a6f57713b6623a1b7
Daniel dos Santos Bossle <daniel.bossle@gmail.com>
2015-03-12 13:02:25 -0300

    Implementando insert_trie

--- src/cc_dict.c
+++ src/cc_dict.c
@@ -13,7 +13,14 @@ trie_dict_item* new_trie_dict_item()
 
 void insert_trie(trie_dict_item* t, char* s, struct comp_dict_item_t* c)
 {
-	t->val=c; //stub
+	int cur_char;
+	for(cur_char=0; s[cur_char]; ++cur_char)
+	{
+		if(t->children[s[cur_char]] == NULL)
+			t->children[s[cur_char]] = new_trie_dict_item();
+		t = t->children[s[cur_char]];
+	}
+	t->val=c;
 }
 
 struct comp_dict_item_t* query_trie(trie_dict_item* t, char* s)
