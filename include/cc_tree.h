977f812a826cb0746d27d728ef94776e78eec15e
Bossle <daniel.bossle@gmail.com>
2015-04-10 23:05:52 -0300

    Changing tree from generic n-ary to 3+n-ary
    Use NULL to indicate inexistent children.
    To reduce trouble of using too many -> =, made a lot of simple functions for initialization

--- include/cc_tree.h
+++ include/cc_tree.h
@@ -1,13 +1,26 @@
 #ifndef __CC_TREE_H__
 #define __CC_TREE_H__
 
+#define NEXT_INVALID 0
+#define NEXT_COMMAND 1
+#define NEXT_ARGUMENT 2
+#define NEXT_OUTPUT 3
+
 struct comp_tree_t {
 	int type;
 	struct comp_dict_item_t* value;
-	struct comp_tree_t *first_child, *next_sibling;
-}
+	
+	struct comp_tree_t* child[3];
+	int next_type;
+	struct comp_tree_t* next;
+};
 
-struct comp_tree_t* new_tree(int type, struct comp_dict_item_t* value, struct comp_tree_t *first_child, struct comp_tree_t *next_sibling);
+struct comp_tree_t* new_tree(int type);
+struct comp_tree_t* new_tree_0(int type, struct comp_dict_item_t* value);
+struct comp_tree_t* new_tree_1(int type, struct comp_tree_t* child0);
+struct comp_tree_t* new_tree_2(int type, struct comp_tree_t* child0, struct comp_tree_t* child1);
+struct comp_tree_t* new_tree_3(int type, struct comp_tree_t* child0, struct comp_tree_t* child1, struct comp_tree_t* child2);
+struct comp_tree_t* new_tree_02(int type, struct comp_dict_item_t* value, struct comp_tree_t* child0, struct comp_tree_t* child1);
 void free_tree(struct comp_tree_t* t);
 
 #endif
--- src/cc_tree.c
+++ src/cc_tree.c
@@ -1,20 +1,67 @@
 #include "cc_tree.h"
 #include <stdlib.h>
 
-struct comp_tree_t* new_tree(int type, struct comp_dict_item_t* value, struct comp_tree_t *first_child, struct comp_tree_t *next_sibling)
+struct comp_tree_t* new_tree(int type)
 {
 	struct comp_tree_t* resp = (struct comp_tree_t*)malloc(sizeof(struct comp_tree_t));
 	resp->type = type;
-	resp->value = value;
-	resp->first_child = first_child;
-	resp->next_sibling = next_sibling;
+	resp->value = NULL;
+	resp->child[0] = NULL;
+	resp->child[1] = NULL;
+	resp->child[2] = NULL;
+	resp->next_type = NEXT_INVALID;
+	resp->next = NULL;
+	return resp;
 }
+
+struct comp_tree_t* new_tree_0(int type, struct comp_dict_item_t* value)
+{
+	struct comp_tree_t* resp = new_tree(type);
+	resp->value = value;
+	return resp;
+}
+
+struct comp_tree_t* new_tree_1(int type, struct comp_tree_t* child0)
+{
+	struct comp_tree_t* resp = new_tree(type);
+	resp->child[0] = child0;
+	return resp;
+}
+
+struct comp_tree_t* new_tree_2(int type, struct comp_tree_t* child0, struct comp_tree_t* child1)
+{
+	struct comp_tree_t* resp = new_tree(type);
+	resp->child[0] = child0;
+	resp->child[1] = child1;
+	return resp;
+}
+
+struct comp_tree_t* new_tree_3(int type, struct comp_tree_t* child0, struct comp_tree_t* child1, struct comp_tree_t* child2)
+{
+	struct comp_tree_t* resp = new_tree(type);
+	resp->child[0] = child0;
+	resp->child[1] = child1;
+	resp->child[2] = child2;
+	return resp;
+}
+
+struct comp_tree_t* new_tree_02(int type, struct comp_dict_item_t* value, struct comp_tree_t* child0, struct comp_tree_t* child1)
+{
+	struct comp_tree_t* resp = new_tree(type);
+	resp->value = value;
+	resp->child[0] = child0;
+	resp->child[1] = child1;
+	return resp;
+}
+
 
 void free_tree(struct comp_tree_t* t)
 {
-	if(t->first_child != NULL)
-		free_tree(t->first_child);
-	if(t->next_sibling != NULL)
-		free_tree(t->next_sibling);
+	int i;
+	for(i=0;i<3;++i)
+		if(t->child[i]!=NULL)
+			free_tree(t->child[i]);
+	if(t->next!=NULL)
+		free_tree(t->next);
 	free(t);
 }
